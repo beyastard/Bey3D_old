085b604e794701309b1446b4cd9e281eb1b92f34
breinhart <bryankreinhart@gmail.com>
2020-03-02 06:38:07 -0600

    014 - add simple timer

---
+++ Engine/Bey3D/include/BeyTimer.h
@@ -0,0 +1,18 @@
+﻿#ifndef __BEY_3D_TIMER_H__
+#define __BEY_3D_TIMER_H__
+
+#include <chrono>
+
+class BeyTimer
+{
+public:
+	BeyTimer();
+
+	float Mark();
+	float Peek() const;
+
+private:
+	std::chrono::steady_clock::time_point last;
+};
+
+#endif // __BEY_3D_TIMER_H__
---
+++ Engine/Bey3D/src/BeyTimer.cpp
@@ -0,0 +1,23 @@
+﻿#include "BeyTimer.h"
+
+using namespace std::chrono;
+
+BeyTimer::BeyTimer()
+{
+	last = steady_clock::now();
+}
+
+float BeyTimer::Mark()
+{
+	const auto old = last;
+	last = steady_clock::now();
+
+	const duration<float> frameTime = last - old;
+
+	return frameTime.count();
+}
+
+float BeyTimer::Peek() const
+{
+	return duration<float>(steady_clock::now() - last).count();
+}
