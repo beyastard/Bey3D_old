6242202f3e33164d4ae9516a29887f70a257b008
breinhart <bryankreinhart@gmail.com>
2020-03-02 03:37:42 -0600

    005 - Added top level try...catch

--- Engine/Bey3D/src/WinMain.cpp
+++ Engine/Bey3D/src/WinMain.cpp
@@ -7,21 +7,37 @@ int CALLBACK WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow)
 {
-	Window wnd(1140, 600, "Bey3D Engine Window");
-	
-	MSG msg;
-	BOOL gResult;
-	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	try
 	{
-		// TranslateMessage will post auxiliary WM_CHAR messages from key msgs
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		Window wnd(1140, 600, "Bey3D Engine Window");
+
+		MSG msg;
+		BOOL gResult;
+		while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+		{
+			// TranslateMessage will post auxiliary WM_CHAR messages from key msgs
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);			
+		}
+
+		// check if GetMessage call itself puked
+		if (gResult == -1)
+			return -1;
+
+		// wParam here is the value passed to PostQuitMessage
+		return int(msg.wParam);
 	}
-
-	// check if GetMessage call itself puked
-	if (gResult == -1)
-		return -1;
-
-	// wParam here is the value passed to PostQuitMessage
-	return int(msg.wParam);
+	catch (const BeyException& e)
+	{
+		MessageBox(nullptr, e.what(), e.GetType(), MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (const std::exception& e)
+	{
+		MessageBox(nullptr, e.what(), "Standard Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (...)
+	{
+		MessageBox(nullptr, "No details available", "Unknown Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	return -1;	
 }
