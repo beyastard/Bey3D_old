6a12fa65881104dd81fd2c67ecc5a867f844cfff
breinhart <bryankreinhart@gmail.com>
2020-03-02 02:49:29 -0600

    002 - Add WindowsMessageMap class

---
+++ Engine/Bey3D/include/WindowsMessageMap.h
@@ -0,0 +1,16 @@
+﻿#ifndef __WINDOWS_MESSAGE_MAP_H__
+#define __WINDOWS_MESSAGE_MAP_H__
+
+#include <unordered_map>
+#include <Windows.h>
+
+class WindowsMessageMap
+{
+public:
+	WindowsMessageMap();
+	std::string operator()(DWORD msg, LPARAM lp, WPARAM wp) const;
+private:
+	std::unordered_map<DWORD, std::string> map;
+};
+
+#endif // __WINDOWS_MESSAGE_MAP_H__
