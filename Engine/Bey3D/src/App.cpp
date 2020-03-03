5222953fd632df6acd5bc7e3d28b981046ba3950
breinhart <bryankreinhart@gmail.com>
2020-03-03 07:23:39 -0600

    023 - add geometric primitives

--- Engine/Bey3D/src/App.cpp
+++ Engine/Bey3D/src/App.cpp
@@ -6,11 +6,11 @@ App::App() : wnd( 1160, 600, "Bey3D Engine Demo" )
 {
 	std::mt19937 rng( std::random_device{}() );
 	std::uniform_real_distribution<float> adist( 0.0f,3.1415f * 2.0f );
-	std::uniform_real_distribution<float> ddist( 0.0f,3.1415f * 2.0f );
-	std::uniform_real_distribution<float> odist( 0.0f,3.1415f * 0.3f );
+	std::uniform_real_distribution<float> ddist(0.0f, 3.1415f * 1.0f);
+	std::uniform_real_distribution<float> odist(0.0f, 3.1415f * 0.08f);
 	std::uniform_real_distribution<float> rdist( 6.0f,20.0f );
 
-	for( auto i = 0; i < 80; i++ )
+	for (auto i = 0; i < 180; i++)
 		boxes.push_back(std::make_unique<Box>(wnd.Gfx(), rng, adist, ddist, odist, rdist));
 
 	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
