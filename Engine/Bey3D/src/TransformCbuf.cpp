f9c71951dffa7ce279d4c5e400bdabbf0ea2ad74
breinhart <bryankreinhart@gmail.com>
2020-03-04 16:00:46 -0600

    031 - Add specular

--- Engine/Bey3D/src/TransformCbuf.cpp
+++ Engine/Bey3D/src/TransformCbuf.cpp
@@ -9,11 +9,11 @@ TransformCbuf::TransformCbuf(Graphics& gfx, const Drawable& parent, UINT slot)
 
 void TransformCbuf::Bind(Graphics& gfx) noexcept
 {
-	const auto model = parent.GetTransformXM();
+	const auto modelView = parent.GetTransformXM() * gfx.GetCamera();
 	const Transforms tf =
 	{
-		XMMatrixTranspose(model),
-		XMMatrixTranspose(model * gfx.GetCamera() * gfx.GetProjection())
+		XMMatrixTranspose(modelView),
+		XMMatrixTranspose(modelView * gfx.GetProjection())
 	};
 	pVcbuf->Update(gfx, tf);
 	pVcbuf->Bind(gfx);
